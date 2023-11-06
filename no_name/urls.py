64ef72ff2479da3632124fea21b6272e3e7017be
Absolem <absolem.jk@gmail.com>
2023-11-06 13:55:39 +0900

    created apps

--- no_name/urls.py
+++ no_name/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.no_name import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
+    path('posts/', include('posts.urls')),
 ]
