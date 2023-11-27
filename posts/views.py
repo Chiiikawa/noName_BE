0848695ea18dada8e0875cd6ffef604f4b960529
GSC <141592320+tkdcks7@users.noreply.github.com>
2023-11-27 13:29:04 +0900

    Fix: PostView logic

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets, status, permissions, generics
-from .models import Post, Like, Comment, GeneratedImage
+from .models import Post, Like, Comment
 from .serializers import LikeSerializer, CommentSerializer, PostCreateSerializer
 from .dalle import generate_image
 from rest_framework.views import APIView
@@ -12,6 +12,7 @@ from django.core.files.base import ContentFile
 import random
 import urllib.request
 from urllib.request import urlopen
+from django.core.files.base import ContentFile
 
 class LikeViewSet(viewsets.ModelViewSet):
     queryset = Like.objects.all()
@@ -30,10 +31,7 @@ class DalleAPIView(APIView):
 
         image_url = generate_image(prompt)  # generate_image 함수를 호출하여 입력된 prompt를 바탕으로 이미지를 생성하고, 생성된 이미지 URL을 image_url 변수에 저장.
         print("image_url은 다음과 같습니다.:", image_url)
-        # 생성된 이미지 정보를 데이터베이스에 저장
-        generated_image = GeneratedImage(prompt=prompt, image_url=image_url, author=request.user)    # GeneratedImage 모델 인스턴스를 생성함, 생성된 이미지의 prompt와 url을 저장함.
-        generated_image.save()
-        return Response({"image": str(image_url)})
+        return Response({"image": str(image_url), "prompt":prompt})
 
 class PostView(APIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -47,28 +45,26 @@ class PostView(APIView):
     def post(self, request):
         print("request.user", request.user.id)
         user = request.user
-        latest_generated_image = GeneratedImage.objects.filter(author=user).last()
-        print("latest_generated_image:", latest_generated_image.id)
-        image_url = latest_generated_image.image_url
+        image_url = request.data["image_url"]
         print("image_url:", image_url)
-        # img_response = requests.get(image_url, stream=True)
+        img_response = requests.get(image_url, stream=True)
         # print("requests로 DALL-E에서 가져온 response:", response)
-        tmp_img = tempfile.NamedTemporaryFile() # 임시파일 생성
-        tmp_img.write(urlopen(image_url).read())
-        print("tmp_img까진 됩니다.")
-
+        # tmp_img = tempfile.NamedTemporaryFile() # 임시파일 생성
+        # tmp_img.write(urlopen(image_url).read())
+        print("img_response까진 됩니다.")
         print("author:", user.id)
-        print("generated_image:", tmp_img)
+        print("img_response", type(img_response.content))
         print("title:", request.data["title"])
         print("content:", request.data["content"])
         
         # author, image, title, content를 post_data라는 변수에 넣음
         post_data = {
             "author": user.id,
-            "generated_image": tmp_img,
             "title": request.data["title"],
             "content": request.data["content"]
         }
+        image_content = ContentFile(img_response.content, name="generated_image.png")
+        post_data["generated_image"] = image_content
 
         # PostCreateSerializer에 post_data를 넣어 validation 후 저장
         post_serializer = PostCreateSerializer(data=post_data)
@@ -80,16 +76,16 @@ class PostView(APIView):
             return Response({"error": post_serializer.errors}, status=400)
 
 # 유저가 마지막으로 생성한 Generated image의 url을 프론트에 보내는 View. 프론트는 이 url을 img tag에 넣어 이미지를 띄운다. 
-# 프론트에서 상태관리를 잘 사용하면 Generatedimage라는 모델과 이 View 없이도 url을 받을 수 있을듯.
-class GetGeneratedImageView(APIView):
-    def get(self, request):
-        user = request.user
-        latest_generated_image = GeneratedImage.objects.filter(author=user).last()
-        print("author:", user.id)
-        if (latest_generated_image):
-            print("generated_image:", latest_generated_image)
-            print("프롬프트:", latest_generated_image.prompt)
-            print(latest_generated_image.image_url)
-            return Response({"image_url": latest_generated_image.image_url})
-        else:
-            return Response({"error": post_serializer.errors}, status=400)
+# 프론트에서 상태관리를 잘 사용하면 Generatedimage라는 모델과 이 View 없이도 url을 받을 수 있어, zustand 사용에 따라 주석처리함.
+# class GetGeneratedImageView(APIView):
+#     def get(self, request):
+#         user = request.user
+#         latest_generated_image = GeneratedImage.objects.filter(author=user).last()
+#         print("author:", user.id)
+#         if (latest_generated_image):
+#             print("generated_image:", latest_generated_image)
+#             print("프롬프트:", latest_generated_image.prompt)
+#             print(latest_generated_image.image_url)
+#             return Response({"image_url": latest_generated_image.image_url})
+#         else:
+#             return Response({"error": post_serializer.errors}, status=400)
