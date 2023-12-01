4fc8178e51c4c4643094607b81aa95cdbc6aa026
lazycaterpillar <absolem.jk@gmail.com>
2023-12-01 14:13:50 +0900

    fix: secret key stored

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
-# docker build -t prjct_noname .
-# docker run --name dockercontainer
 From python:3.11-slim
 
 ENV PYTHONUNBUFFERED=1
 RUN apt update
+
 
 # 가상환경 설치 && 실행
 RUN python3 -m venv myvenv
@@ -11,7 +10,7 @@ RUN echo "source myvenv/bin/activate"
 
 # workspace 폴더 생성
 RUN mkdir -p /srv/workspace/noName_BE
-WORKDIR /srv/workspace/no Name_BE
+WORKDIR /srv/workspace/noName_BE
 # WORKDIR 이동
 
 
@@ -20,8 +19,7 @@ WORKDIR /srv/workspace/no Name_BE
 COPY . .
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
+RUN pip install psycopg2-binary
 
 
-# nginx 사용할거면 80번 포트로 바꿔주기...
 EXPOSE 8000
-CMD ["python3", "./manage.py", "runserver", "0.0.0.0:8000"]
---
+++ posts/serializers.py
@@ -0,0 +1,75 @@
+from rest_framework import serializers
+from .models import Like, Comment, Post
+
+#BaseInteractionSerializer을 만들어서 아래 LikeSerializer,CommentSerializer에서 중복값을 줄임
+class BaseInteractionSerializer(serializers.ModelSerializer):
+    author = serializers.SerializerMethodField()
+    class Meta:
+        abstract = True
+
+class LikeSerializer(BaseInteractionSerializer):
+    class Meta(BaseInteractionSerializer.Meta):
+        model = Like
+        fields = ['id', 'user', 'post']
+        
+class CommentSerializer(BaseInteractionSerializer):
+    class Meta(BaseInteractionSerializer.Meta):
+        model = Comment
+        fields = ['id', 'post', 'user', 'content', 'created_at']
+
+#CommentCreateSerializer는 CommentSerializer을 종속받아 중복값을 줄임
+class CommentCreateSerializer(CommentSerializer):
+    class Meta(CommentSerializer.Meta):
+        fields = ("post", "content")
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['author', 'title', 'content', 'generated_image']
+
+        def validate_generated_image(self, value):
+        # Ensure that a generated image is provided
+            if not value:
+                raise serializers.ValidationError("A generated image is required.")
+            return value
+
+class PostListSerializer(serializers.ModelSerializer):
+    author = serializers.SerializerMethodField()
+
+    def get_author(self, obj):
+        return {
+            "id": obj.author.pk,
+            "nickname": obj.author.nickname,
+        }
+
+    class Meta:
+        model = Post
+        fields = (
+            "id",
+            "author",
+            "generated_image",
+            "created_at",
+        )
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    author = serializers.SerializerMethodField()
+    likes = LikeSerializer(many=True, read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+
+    def get_author(self, obj):
+        return {
+            "id": obj.author.pk,
+            "nickname": obj.author.nickname,
+        }
+
+    class Meta:
+        model = Post
+        fields = (
+            "generated_image",
+            "author",
+            "title",
+            "content",
+            "likes",
+            "comments",
+        )
